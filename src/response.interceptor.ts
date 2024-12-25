0102b10198bc763a259c925c2849024caf57a89b
jidan745le <420511176@qq.com>
2024-12-26 01:22:08 +0800

    feat:加logger记录接口处理时间

--- src/response.interceptor.ts
+++ src/response.interceptor.ts
@@ -1,44 +1,53 @@
 import {
-    Injectable,
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler,
-  } from '@nestjs/common';
-  import { map } from 'rxjs/operators';
-  import { catchError, Observable, throwError,of } from 'rxjs';
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { catchError, Observable, throwError, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-  @Injectable()
-  export class ResponseInterceptor implements NestInterceptor {
-    intercept(
-      context: ExecutionContext,
-      next: CallHandler,
-    ): Observable<any> {
-      return next.handle().pipe(
-        catchError(err => {
-             console.log('err212312312', err)
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  private readonly logger = new Logger('HTTP');
 
-            if(err.getStatus && err.getStatus() === 200){
-                return of({message:err.getResponse(),error:true})
-            }
-            return throwError(() => err)
-          }),
-        map((data) => {
-          // 在这里对返回的数据进行包装
-          if(data?.message && data?.error){
-            return { 
-                success: false,
-                message: data.message,
-                code:200,
-                timestamp: new Date().toISOString(), // 添加时间戳
-            }
-          }
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const startTime = Date.now();
+    const request = context.switchToHttp().getRequest();
+    const { method, url } = request;
+
+    return next.handle().pipe(
+      catchError((err) => {
+        console.log('err212312312', err);
+
+        if (err.getStatus && err.getStatus() === 200) {
+          return of({ message: err.getResponse(), error: true });
+        }
+        return throwError(() => err);
+      }),
+      map((data) => {
+        if (data?.message && data?.error) {
           return {
-            success: true,
-            data: data, // 原始数据
-            code:200,
-            timestamp: new Date().toISOString(), // 添加时间戳
+            success: false,
+            message: data.message,
+            code: 200,
+            timestamp: new Date().toISOString(),
           };
-        }),
-      );
-    }
+        }
+        return {
+          success: true,
+          data: data,
+          code: 200,
+          timestamp: new Date().toISOString(),
+        };
+      }),
+      tap(() => {
+        const processingTime = Date.now() - startTime;
+        console.log('processingTime', processingTime);
+        this.logger.log(`${method} ${url} - ${processingTime}ms`);
+      }),
+    );
   }
+}
